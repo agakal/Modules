453d9e22b04ae8d4012830b21cb42f6784cdec84
agakal <76710736+agakal@users.noreply.github.com>
2021-02-10 21:08:20 +0100

    Add files via upload

---
+++ modul_oświetlenia/Debug/czujniki.cpp.d
@@ -0,0 +1,27 @@
+C:\Users\l\AppData\Local\Temp\VMBuilds\czujniki\uno\Debug\czujniki.cpp.o: \
+ C:\Users\l\AppData\Local\Temp\VMBuilds\czujniki\uno\Debug\czujniki.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h \
+ c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\ex1pb14w.0vm\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\l\AppData\Local\Temp\VMBuilds\czujniki\uno\Debug\DHT.h \
+ C:\Users\l\Documents\Arduino\libraries\DFRobot_CCS811-master/DFRobot_CCS811.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\l\Documents\Arduino\libraries\Adafruit_Unified_Sensor/Adafruit_Sensor.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\l\Documents\Arduino\libraries\Adafruit_TSL2591_Library/Adafruit_TSL2591.h \
+ C:\Users\l\Documents\Arduino\libraries\LiquidCrystal\src/LiquidCrystal.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SoftwareSerial\src/SoftwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\SPI\src/SPI.h \
+ C:\Users\l\Documents\Arduino\libraries\NRFLite\src/NRFLite.h \
+ C:\Users\l\Documents\Arduino\libraries\NRFLite\src/nRF24L01.h
---
+++ modul_oświetlenia/modul_oswietleni/modul_oswietleni.ino
@@ -0,0 +1,69 @@
+﻿#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+
+// Konfiguracja odbiornika
+
+RF24 radio(9, 10); // radio (CE,CSN)
+RF24Network network(radio);  //tworzenie sieci radiowej na podstawie skonfigurowanej komunikacji
+const uint16_t this_node = 03; // Adres tego modułu w (zapisany oktalnie)
+const uint16_t other_node = 00; // Adres modułu sterującego
+
+unsigned short data[4];
+unsigned short light[2];
+void setup() {
+    Serial.begin(9600); // rozpoczęcie pracy portu szeregowego - ustwienie prędkoęci transmisji danych na 9600 BAUD
+    SPI.begin(); // ustawienie wyj�ć na SCK, MOSI i CE; stanu niskiego na SCK i MOSI; wysokiego na CE 
+    radio.begin(); // sprawdzenie czy został skonfigurowany interfejs SPI, ustawienie CSN jako wejście 
+    network.begin(90, this_node); // (kanał, adres tego modułu)
+    radio.setDataRate(RF24_2MBPS); // ustawienie prędkości transmisji radiowej
+    pinMode(4, OUTPUT); // Pin D4 - odpowiedzialny za zmianę stanu na wyjściu przekaźnika 
+    digitalWrite(4, LOW); // Przekaźnik ma styk NO (normalnie otwarty)
+
+}
+unsigned long lastRecvTime = 0;
+
+void radioread()
+{
+    network.update(); // sprawdza czy przesłano paczki danych
+    while (network.available()) // odczytuje "header" wiadomości
+    {
+        RF24NetworkHeader header1;
+        radio.setDataRate(RF24_2MBPS);
+        network.read(header1, &data, sizeof(data));
+    }
+}
+
+void check() // porównanie danych z obednej i koljnej wiadomości - wykrycie zbocza naastaj�cego/opadaj�cego
+{
+    if ((millis() - lastRecvTime) < 5000)
+    {
+        data[1] = light[0];
+    }
+    else if (((millis() - lastRecvTime) > 5000))
+    {
+        data[1] = light[1];
+        lastRecvTime = millis();
+    }
+}
+void change()
+{
+    if (light[1] == light[0])
+    {
+        if (light[1] == 1) // (natężenie światła jest zbyt niskie)
+        {
+            digitalWrite(4, HIGH);
+        }
+        else if (light[1] == 0) // (natężenie światła w normie)
+        {
+            digitalWrite(4, LOW);
+        }
+    }
+}
+
+void loop()
+{
+    radioread();
+    check();
+    change();
+}
